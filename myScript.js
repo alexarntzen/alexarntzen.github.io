09fc53074176145080f33c800a4a21d0c56b6ff3
Alexander Johan Arntzen <alexander.arntzen@gmail.com>
2021-05-24 14:58:03 +0200

    bugfix

--- myScript.js
+++ myScript.js
@@ -3,11 +3,11 @@ let nameArray = [];
 document.querySelector(".button").addEventListener("click", () => {
     let name =document.querySelector("#nameInput").value;
     nameArray.push(name);
-    document.querySelector("#kolonnetext2").textContent =  "Hei " + name + "!";
+    document.querySelector("#kolonnetext2").textContent =  "Hey, " + name + "!";
     document.querySelector("#kolonnetext3").innerHTML = "";
     let i;
     for (i = 0; i < nameArray.length; i++) {
-        let text = document.createTextNode("Hei " +  nameArray[i] + "!");
+        let text = document.createTextNode("Hey, " +  nameArray[i] + "!");
         let br = document.createElement("br");
         document.querySelector("#kolonnetext3").appendChild(text);
         document.querySelector("#kolonnetext3").appendChild(br);
