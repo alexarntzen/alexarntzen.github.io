4c7cd45144f385ca5830cbbef33a3a6a13426312
Alexander Johan Arntzen <alexander.arntzen@gmail.com>
2021-05-29 17:42:40 +0200

    added better head and some other fixes

--- myScript.js
+++ myScript.js
@@ -1,13 +1,13 @@
 let nameArray = [];
 
 document.querySelector(".button").addEventListener("click", () => {
-    let name =document.querySelector("#nameInput").value;
+    let name = document.querySelector("#nameInput").value;
     nameArray.push(name);
-    document.querySelector("#kolonnetext2").textContent =  "Hey, " + name + "!";
+    document.querySelector("#kolonnetext2").textContent = "Hey, " + name + "!";
     document.querySelector("#kolonnetext3").innerHTML = "";
     let i;
     for (i = 0; i < nameArray.length; i++) {
-        let text = document.createTextNode("Hey, " +  nameArray[i] + "!");
+        let text = document.createTextNode("Hey, " + nameArray[i] + "!");
         let br = document.createElement("br");
         document.querySelector("#kolonnetext3").appendChild(text);
         document.querySelector("#kolonnetext3").appendChild(br);
